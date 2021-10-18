65ca01a83255d82db99a01288aac4205cbab0cad
ClydeRM <clyde.official@icloud.com>
2021-10-18 15:55:28 +0800

    Combine validator and verify user login registry

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,20 +1,84 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
 
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../util/validators");
 const { SECRET_KEY } = require("../../config");
 const User = require("../../model/User");
 
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+}
+
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+      if (!valid) {
+        throw new UserInputError("Error", { errors });
+      }
+      const user = await User.findOne({ username });
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong credentials";
+        throw new UserInputError("Wrong credentials", { errors });
+      }
+      const token = generateToken(user);
+
+      return {
+        // 回傳 使用者user 資料 id token 給 client
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
+
     async register(
       _,
-      { registerInput: { username, email, password, confirmPassword } },
-      context,
-      info
+      { registerInput: { username, email, password, confirmPassword } }
     ) {
-      // TODO: Validate user Data
-      // TODO: Makes sure doesn't already exist
-      // TODO: hash password and create an auth token
+      // Validate user Data
+      const { errors, valid } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Error", { errors });
+      }
+      // Makes sure doesn't already exist
+      const user = await User.findOne({ username });
+      const userEmail = await User.findOne({ email });
+      if (user) {
+        throw new UserInputError("Username is taken", {
+          errors: {
+            username: "This username is taken",
+          },
+        });
+      } else if (userEmail) {
+        throw new UserInputError("Email is taken", {
+          errors: {
+            username: "This email is taken",
+          },
+        });
+      }
+      //  hash password and create an auth token
       password = await bcrypt.hash(password, 12);
       const newUser = new User({
         email,
@@ -25,20 +89,13 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        SECRET_KEY,
-        { expiresIn: "1h" }
-      );
+      const token = generateToken(res);
 
       return {
-          ...res._doc,
-          id: res._id,
-          token
+        // 回傳 使用者res 資料 id token 給 client
+        ...res._doc,
+        id: res._id,
+        token,
       };
     },
   },
