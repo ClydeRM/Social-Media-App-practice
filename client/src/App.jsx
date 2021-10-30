7eb3c55af53d1eabd994e051ce4c8a84029de93e
ClydeRM <clyde.official@icloud.com>
2021-10-30 14:31:10 +0800

    Use Router and setup MenuBar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,27 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.css"; // import custom css after semantic ui css can override semantic ui
+
+import MenuBar from "./components/MenuBar";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hi World</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/login" component={Login}></Route>
+        <Route exact path="/register" component={Register}></Route>
+      </Container>
+    </Router>
   );
-}
+};
 
 export default App;
