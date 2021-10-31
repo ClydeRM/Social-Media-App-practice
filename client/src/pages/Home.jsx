e54bf6235bd7287ac7934b3bd337bf302d2e4657
ClydeRM <clyde.official@icloud.com>
2021-10-31 11:38:41 +0800

    Home page to show all posts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,58 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import { Grid } from "semantic-ui-react";
+
+import PostCard from "../components/PostCard";
 
 const Home = () => {
+  // destructure the data to only get Posts data array object
+  // const { loading, data:{getPosts: posts} } = useQuery(FETCH_POSTS_QUERY); getError getPosts undefined?
+
+  const { loading, data} = useQuery(FETCH_POSTS_QUERY);
+  const { getPosts: posts } = data || {};
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading posts..</h1>
+        ) : (
+          posts &&
+          posts.map((post) => (
+            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
   );
 };
+
+const FETCH_POSTS_QUERY = gql`
+  query getPosts {
+    getPosts {
+      id
+      username
+      body
+      createdAt
+      likeCount
+      likes {
+        id
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        body
+      }
+    }
+  }
+`;
 
 export default Home;
