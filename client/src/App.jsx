671d4ffbe4003b5c62fdd7868c649a3a2f26c7ce
ClydeRM <clyde.official@icloud.com>
2021-11-03 15:31:32 +0800

    Import and use AuthContextProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,35 @@
+// package
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 
+// style
 import "semantic-ui-css/semantic.min.css";
 import "./App.css"; // import custom css after semantic ui css can override semantic ui
 
+// context
+import { AuthProvider } from "./context/auth";
+
+// component
 import MenuBar from "./components/MenuBar";
 
+// page
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 const App = () => {
   return (
-    <Router>
-      <Container>
-        <MenuBar />
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/login" component={Login}></Route>
-        <Route exact path="/register" component={Register}></Route>
-      </Container>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Container>
+          <MenuBar />
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/login" component={Login}></Route>
+          <Route exact path="/register" component={Register}></Route>
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 };
 
