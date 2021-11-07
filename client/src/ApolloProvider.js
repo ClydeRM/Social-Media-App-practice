62c737e2498b5aa99c809d39d6528367f9bf49d8
ClydeRM <clyde.official@icloud.com>
2021-11-07 15:24:06 +0800

    Install apollo-link-context for a AuthHeader

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -4,14 +4,30 @@ import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { setContext } from "apollo-link-context";
 
+// Link to gql server
 const httpLink = createHttpLink({
   uri: "http://localhost:5000",
 });
 
+// Use authorization token in header
+// Work like a middleware to handle requests package
+// setContext((req, previous_context) => {...} ), req is requests package, previous_context is forwarding to next middleware
+// In the case , we don't need to setting detail, so leave it empty
+const authLink = setContext(() => {
+  const token = localStorage.getItem('jwtToken');
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+// Create ApolloClient
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink), // httpLink is a string array, concat the headers setting string in original string array
+  cache: new InMemoryCache(), // 4:00:00 direct access client cache
 });
 
 export default (
