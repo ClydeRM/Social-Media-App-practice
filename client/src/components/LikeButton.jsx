4b362c8983a6449f0e0f608ec6ce4ff3be40a829
ClydeRM <clyde.official@icloud.com>
2021-11-07 15:34:00 +0800

    Build LikeButton component handle user likePost

---
+++ client/src/components/LikeButton.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import { Button, Icon, Label } from "semantic-ui-react";
+
+
+const LikeButton = ({user, post: { id, likes, likeCount } }) => {
+  const [liked, setLiked] = useState(false);
+
+  useEffect(() => {
+    if (user && likes.find((like) => like.username === user.username)) {
+      // user login and have liked this post
+      setLiked(true);
+    } else {
+      setLiked(false);
+    }
+  }, [user, likes]);
+
+  const [likePost] = useMutation(LIKE_POST_MUTATION, {
+      variables: {postId: id}
+  })
+
+  const likeButton = user ? (
+    // user login
+    liked ? (
+      // Have liked this post, show full box
+      <Button color="teal">
+        <Icon name="heart" />
+      </Button>
+    ) : (
+      // Haven't liked this post, show basic box
+      <Button color="teal" basic>
+        <Icon name="heart" />
+      </Button>
+    )
+  ) : (
+    // User not login, button as a link to login page
+    <Button as={Link} to="/login" color="teal" basic>
+      <Icon name="heart" />
+    </Button>
+  );
+
+  return (
+    <Button as="div" labelPosition="right" onClick={likePost}>
+      {likeButton}
+      <Label basic color="teal" pointing="left">
+        {likeCount}
+      </Label>
+    </Button>
+  );
+};
+
+
+const LIKE_POST_MUTATION = gql`
+    mutation likePost($postId: ID!){
+        likePost(postId: $postId){
+            id
+            likes{
+                id
+                username
+            }
+            likeCount
+        }
+    }
+`;
+
+export default LikeButton;
